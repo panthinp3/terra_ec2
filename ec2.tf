6bec81f8b8dcd121c810e3fdc594bc6789bcf4bb
panthinp3 <57723973+panthinp3@users.noreply.github.com>
2021-06-28 17:24:50 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,6 @@
 resource "aws_instance" "ec2-01" {
     ami= "ami-0d5eff06f840b45e9"
     region= "us-east-1"
-    availability_zone = "us-east-1a"
     instance_type = "t2.micro"
     tags ={
         Name= "Jenkins-demo"
