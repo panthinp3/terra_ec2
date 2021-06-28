7fe5fe947eaaec0c39151a61ef58f6e7ca53e2e1
panthinp3 <57723973+panthinp3@users.noreply.github.com>
2021-06-28 17:24:18 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,7 @@
 #creating aws instance
 resource "aws_instance" "ec2-01" {
     ami= "ami-0d5eff06f840b45e9"
+    region= "us-east-1"
     availability_zone = "us-east-1a"
     instance_type = "t2.micro"
     tags ={
