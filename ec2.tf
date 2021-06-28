7df8b5970acf99cb51b2a18deb0359dd17248a01
panthinp3 <57723973+panthinp3@users.noreply.github.com>
2021-06-28 16:57:27 -0500

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,9 @@
+#creating aws instance
+resource "aws_instance" "ec2-01" {
+    ami= "ami-0d5eff06f840b45e9"
+    availability_zone = "us-east-1a"
+    instance_type = "t2.micro"
+    tags ={
+        Name= "Dev"
+        }
+    }
